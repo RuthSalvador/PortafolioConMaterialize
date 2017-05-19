aca991cd876f56c3bb6668a058ef182368dd6c26
Pancake <sandra_sv01@outlook.com>
2017-05-19 11:39:30 -0500

    Validaciones JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,26 @@
 //Contact validations
 window.onload = function(){
-  var fist_name = document.getElementById('first_name');
-  var last_name = document.getElementById('last_name');
-  var message   = document.getElementById('textarea1');
+  var input   = document.getElementsByClassName('input');
+  var error   = document.getElementsByClassName('error');
 
-  var letras = function (e) {
+  var soloLetras = function (e) {
     var codLetra = e.keyCode;
-    if ((codLetra>=97 && codLetra<=122) || (codLetra>=65 && codLetra<=90) || codLetra == 39 || codLetra == 32 || codLetra==241 || codLetra==209) {
-      return true;
+    if((codLetra>=97 && codLetra<=122) || (codLetra>=65 && codLetra<=90) ||
+      codLetra == 39 || codLetra == 32 || codLetra==241 || codLetra==209) {
+      return true
     } else {
-      return false;
+      return false
     }
   }
 
-  first_name.onkeypress = letras;
-  last_name.onkeypress = letras;
+  input[0].onkeypress = soloLetras;
+  input[1].onkeypress = soloLetras;
+
+  document.getElementById('submit').addEventListener('click',function(){
+    if(input[0].value==''||input[1].value==''||input[2].value==''||input[3].value==''){
+      error[0].textContent = 'Todos los campos son obligatorios';
+    } else {
+      error[0].textContent = '';
+    }
+  });
 }
